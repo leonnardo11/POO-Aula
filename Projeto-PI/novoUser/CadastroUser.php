ed7d6d6920a25c7756f2ba46805b4ce499ecd79d
Leonardo Rodrigues <54442873+leonnardo11@users.noreply.github.com>
2022-06-10 14:46:19 -0300

    aula/05-04-22
    Correção.

---
+++ Projeto-PI/novoUser/CadastroUser.php
@@ -0,0 +1,60 @@
+<?php 
+
+
+
+// require_once('sessao/controle.php');
+require_once('../banco/conecta.php');
+require_once('../classes/Usuario.class.php');
+
+
+$nome = $_POST['nome'] ?? false;
+$sobrenome = $_POST['sobrenome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$confirma = $_POST['confirma'];
+
+class CadastroUser{
+
+    public function __construct($bd){
+
+        $user = new Usuario($bd);
+
+        if(!$user->saoIguais($_POST['senha'], $_POST['confirma'])){
+            echo"A senhas e sua confirmação não conferem";
+            exit();
+        }
+
+        $saida = $user->cadastrar($_POST);
+
+        if($saida == true){
+
+            header('Location: ../telas/formlogin.php'); 
+
+        }else{
+
+            switch($saida){
+
+                case 1:
+                    echo $saida[1];
+                    break;
+
+                case 2:
+                    echo $saida[2];
+                    break;
+                case 3:
+                    echo $saida[3];
+                    break;    
+
+                default:
+                    echo"Erro desconhecido";      
+            }
+        }
+
+
+    }
+}
+
+new CadastroUser($bd);
+
+
+
